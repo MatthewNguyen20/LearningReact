1b6a695bbc1d897df103bf01c770c81ac665c151
Matthew <matthewnguyen156@gmail.com>
2024-06-21 18:35:51 -0500

    Experimented with React components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [numberOfClicks, setNumberOfClicks] = useState(0);
+
+  const mainParagraph = {
+    fontSize: 130/2,
+    backgroundColor: "red",
+    padding: "10rem"
+  };
+  const buttonStyle = {
+    backgroundColor: "green",
+    color: "white",
+    margin: 25
+  };
+  
+  const handleOnClick = () => { 
+    setNumberOfClicks(numberOfClicks + 1); 
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style={mainParagraph}>
+          Matthew
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button style={buttonStyle} type="button" onClick={handleOnClick}>Click Me!</button>
+        <div><p>{numberOfClicks}</p></div>
       </header>
     </div>
   );
